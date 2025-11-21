ef8f5f5c36a7e25eaa308c263ee76d43ad5d75b5
Shaedow2000 <amsal8795@gmail.com>
2025-11-21 08:49:45 +0100

    Add transitions to all body tag elements.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ const xmark = document.getElementById( 'xmark' ) as HTMLImageElement;
 const menu = document.getElementById( 'menu' ) as HTMLImageElement;
 
 xmark.addEventListener( 'click', (): void => {
-  aside.style.left = '-900px';
+  aside.style.left = '-400px';
 } );
 
 menu.addEventListener( 'click', (): void => {
