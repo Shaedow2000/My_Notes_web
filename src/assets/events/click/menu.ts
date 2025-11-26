31c70f4b6e8dff75e1d1b4b9cc8b2b7e5988b734
Shaedow2000 <amsal8795@gmail.com>
2025-11-26 13:09:58 +0100

    Create new event/click directory for better organization.

---
+++ src/assets/events/click/menu.ts
@@ -0,0 +1,14 @@
+const aside = document.getElementById( 'aside' ) as HTMLDivElement;
+const xmark = document.getElementById( 'xmark' ) as HTMLImageElement;
+const menu = document.getElementById( 'menu' ) as HTMLImageElement;
+const shadow_bg = document.getElementById( 'shadow-bg' ) as HTMLDivElement;
+
+xmark.addEventListener( 'click', (): void => {
+  aside.style.left = '-600px';
+  shadow_bg.style.display = 'none';
+} );
+
+menu.addEventListener( 'click', (): void => {
+  aside.style.left = '0px';
+  shadow_bg.style.display = 'block';
+} );
